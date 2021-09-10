c201afeb715ce9eb70b2a36a8a6a15ccaf8e6619
Ali Asadi <aliesaassadi@gmail.com>
2021-09-10 20:13:53 +0300

    merge sorted arrays

---
+++ src/data_structure/arrays/questions/MergeSortedArrays.java
@@ -0,0 +1,79 @@
+package data_structure.arrays.questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeSortedArrays {
+
+    //Create a function that merge sorted arrays
+    // input [0,3,4,31], [4,6,30]
+    // output [0, 3, 4, 4, 6, 30, 31]
+
+    // Questions:
+    // 1. The input could be null? or empty?
+    // 2. Are they only integer or could be float? negate values?
+    // 3. In case one of both null what we should return? or if one is null to return the second?
+    public static void main(String[] args) {
+        List<Integer> array = mergeSortedArrays(Arrays.asList(0, 3, 4, 31), Arrays.asList(4, 6, 30));
+//        List<Integer> array = mergeSortedArrays(Arrays.asList(0, 3, 4, 31, 177), Arrays.asList(4, 6, 30, 55, 67, 70, 111, 200, 300));
+//        List<Integer> array = mergeSortedArrays(Arrays.asList(0, 3, 4, 31, 177), new ArrayList<>());
+//        List<Integer> array = mergeSortedArrays( new ArrayList<>(), Arrays.asList(4, 6, 30));
+//        List<Integer> array = mergeSortedArrays(null, Arrays.asList(4, 6, 30, 55, 67, 70, 111, 200, 300));
+//        List<Integer> array = mergeSortedArrays(null, null);
+        System.out.println(array);
+    }
+
+    //1. Check inputs
+    //
+    // BigO:
+    // Time = O(n)
+    // Space = O(n)
+    public static List<Integer> mergeSortedArrays(List<Integer> array1, List<Integer> array2) {
+        ArrayList<Integer> mergedSortedArray = new ArrayList<>();
+
+        if (array1 != null && array2 != null) {
+            int index1 = 0;
+            int index2 = 0;
+
+            while (index1 < array1.size()) {
+                int num1 = array1.get(index1);
+
+                if (index2 < array2.size()) {
+                    int num2 = array2.get(index2);
+
+                    if (num1 < num2) {
+                        mergedSortedArray.add(num1);
+                        index1++;
+                    } else {
+                        mergedSortedArray.add(num2);
+                        index2++;
+                    }
+                } else {
+                    //If the second array is empty, add all of the first array's left items to the list.
+                    mergedSortedArray.add(num1);
+                    index1++;
+                }
+            }
+
+            //If the second array is larger than the first and there are items left in it, add them to the list.
+            if (index2 < array2.size()) {
+                for (int i = index2; i < array2.size(); i++) {
+                    mergedSortedArray.add(array2.get(i));
+                }
+            }
+        } else {
+            if (array1 == null && array2 == null) {
+                return mergedSortedArray;
+            } else {
+                if (array1 == null) {
+                    return array2;
+                } else {
+                    return array1;
+                }
+            }
+        }
+
+        return mergedSortedArray;
+    }
+}
