919040114f9806f8d58d28567925931bb9dc4a0c
Ali Asadi <aliesaassadi@gmail.com>
2021-10-11 23:37:25 +0300

    update

---
+++ src/algorithms/sort/MergeSort.java
@@ -0,0 +1,15 @@
+package algorithms.sort;
+
+/**
+ * Created by Ali Asadi on 11/10/2021
+ */
+public class MergeSort {
+
+    static void sort(int[] arr) {
+        //TODO
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
