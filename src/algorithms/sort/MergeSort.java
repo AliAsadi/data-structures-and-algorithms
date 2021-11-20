b27e2e50c2903942f246bc3ceb9425430a79de63
Ali Asadi <aliesaassadi@gmail.com>
2021-11-20 16:14:52 +0200

    update

--- src/algorithms/sort/MergeSort.java
+++ src/algorithms/sort/MergeSort.java
@@ -1,17 +1,57 @@
 package algorithms.sort;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 /**
  * Created by Ali Asadi on 11/10/2021
  */
 public class MergeSort {
 
-    static void sort(int[] arr) {
-        //TODO
+    static int[] sort(int[] arr) {
+        if (arr.length == 1) return arr;
+
+        int size = arr.length;
+        int mid = size / 2;
+        int[] leftArr = Arrays.copyOfRange(arr, 0, mid);
+        int[] rightArr = Arrays.copyOfRange(arr, mid, size);
+
+        leftArr = sort(leftArr);
+        rightArr = sort(rightArr);
+
+        int mergedArraySize = leftArr.length + rightArr.length;
+        int[] mergedArray = new int[mergedArraySize];
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        for (int i = 0; i < mergedArraySize; i++) {
+            if (isBothArraysStillHaveValues(leftIndex, rightIndex, leftArr, rightArr)) { // There is value in the both array sides
+                if (leftArr[leftIndex] < rightArr[rightIndex]) {
+                    mergedArray[i] = leftArr[leftIndex++];
+                } else {
+                    mergedArray[i] = rightArr[rightIndex++];
+                }
+            } else if (isArrayStillHaveValue(leftIndex, leftArr)) { // Get lifted value from the left array
+                mergedArray[i] = leftArr[leftIndex++];
+            } else if (isArrayStillHaveValue(rightIndex, rightArr)) { //Get lifted value from the right array
+                mergedArray[i] = rightArr[rightIndex++];
+            }
+        }
+
+        return mergedArray;
+    }
+
+    private static boolean isBothArraysStillHaveValues(int leftIndex, int rightIndex, int[] leftArr, int[] rightArr) {
+        return (leftIndex < leftArr.length) && (rightIndex < rightArr.length);
+    }
+
+    private static boolean isArrayStillHaveValue(int index, int[] arr) {
+        return index < arr.length;
     }
 
     public static void main(String[] args) {
+        int[] arr = {4, 6, 3, 2};
+        System.out.println(Arrays.toString(sort(arr)));
 
     }
 }
