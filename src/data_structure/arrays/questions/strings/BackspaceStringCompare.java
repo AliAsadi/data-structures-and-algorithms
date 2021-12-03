847f15d24a3a9462ac692bf9073fc4f6a154787c
Ali Asadi <aliesaassadi@gmail.com>
2021-12-03 20:55:07 +0200

    update

--- src/data_structure/arrays/questions/strings/BackspaceStringCompare.java
+++ src/data_structure/arrays/questions/strings/BackspaceStringCompare.java
@@ -3,18 +3,21 @@ package data_structure.arrays.questions.strings;
 import java.util.Stack;
 
 /**
- * Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+ * Given two strings s and t, return true if they are equal when both are typed into empty text editors.
+ * '#' means a backspace character.
  * Note that after backspacing an empty text, the text will continue empty.
- * <p>
+ *
  * Input: s = "ab#c", t = "ad#c"
  * Output: true
  * Explanation: Both s and t become "ac".
- * <p>
+ *
  * Input: s = "ab##", t = "c#d#"
  * Output: true
  * Explanation: Both s and t become "".
- * <p>
+ *
  * Follow up: Can you solve it in O(n) time and O(1) space?
+ *
+ * https://leetcode.com/problems/backspace-string-compare/
  */
 public class BackspaceStringCompare {
     public static boolean backspaceCompare(String s, String t) {
