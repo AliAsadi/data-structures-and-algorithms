6004d3dc0af0bcf0320f7ca4f5d3b22349035b78
Ali Asadi <aliesaassadi@gmail.com>
2021-12-04 01:03:48 +0200

    update

---
+++ src/data_structure/arrays/questions/strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,102 @@
+package data_structure.arrays.questions.strings;
+
+import data_structure.hash_table.HashMap;
+
+import java.util.HashSet;
+
+/**
+ * Given a string s, find the length of the longest substring without repeating characters.
+ * <p>
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * <p>
+ * Input: s = "bbbbb"
+ * Output: 1
+ * <p>
+ * Input: s = "pwwkew"
+ * Output: 3
+ * <p>
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static int lengthOfLongestSubstring(String s) {
+        char[] charArray = s.toCharArray();
+        if (charArray.length == 0) return 0;
+        int maxLength = 0;
+
+        int indexA = 0;
+        int indexB = 1;
+
+
+        HashSet<Character> characters = new HashSet<>();
+
+        while (indexA < indexB) {
+            int newMax = 0;
+            characters.add(charArray[indexA]);
+
+            if (characters.contains(charArray[indexB])) {
+                characters.remove(charArray[indexA]);
+                newMax = indexB - indexA;
+
+                indexA++;
+                if (indexA == indexB) {
+                    characters.add(charArray[indexB]);
+                    indexB++;
+                }
+            } else {
+                characters.add(charArray[indexB]);
+                indexB++;
+            }
+
+
+
+            if (indexB >= charArray.length) {
+                newMax = indexB - indexA;
+                return Math.max(newMax, maxLength);
+            }
+
+            maxLength = Math.max(newMax, maxLength);
+        }
+
+
+        return maxLength;
+    }
+
+    /**
+     * Time: O(N^2)
+     * Space: O(N)
+     **/
+    public static int lengthOfLongestSubstring2(String s) {
+        char[] charArray = s.toCharArray();
+        int maxLength = 0;
+
+
+        for (int i = 0; i < charArray.length; i++) {
+            int newMax = 1;
+
+            HashSet<Character> characters = new HashSet<>();
+            characters.add(charArray[i]);
+
+            for (int j = i + 1; j < charArray.length; j++) {
+                if (characters.contains(charArray[j])) {
+                    break;
+                } else {
+                    characters.add(charArray[j]);
+                    newMax++;
+                }
+            }
+            maxLength = Math.max(newMax, maxLength);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbbabcde"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring(""));
+    }
+}
