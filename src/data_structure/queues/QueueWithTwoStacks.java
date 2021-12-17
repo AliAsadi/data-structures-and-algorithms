a46a636ba3b915d55cc78bae22962c3a6129e1a8
Ali Asadi <aliesaassadi@gmail.com>
2021-12-17 18:41:13 +0200

    update

--- src/data_structure/queues/QueueWithTwoStacks.java
+++ src/data_structure/queues/QueueWithTwoStacks.java
@@ -5,8 +5,8 @@ import data_structure.stacks.Stack;
 import java.util.*;
 
 public class QueueWithTwoStacks<T> {
-    private final data_structure.stacks.Stack<T> in = new data_structure.stacks.Stack<>();
-    private final data_structure.stacks.Stack<T> out = new Stack<>();
+    private final Stack<T> in = new Stack<>();
+    private final Stack<T> out = new Stack<>();
 
     public void add(T item) {
         in.push(item);
