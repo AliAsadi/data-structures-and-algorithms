dcc8a5c4b68c9739808432b0e83f7b749f21f0cb
Ali Asadi <aliesaassadi@gmail.com>
2021-12-17 19:04:16 +0200

    update

--- src/data_structure/queues/questions/Permutation.java
+++ src/data_structure/queues/questions/Permutation.java
@@ -5,15 +5,10 @@ import data_structure.queues.RandomizedQueue;
 public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
-        randomizedQueue.enqueue("A");
-        randomizedQueue.enqueue("B");
-        randomizedQueue.enqueue("C");
-        randomizedQueue.enqueue("D");
-        randomizedQueue.enqueue("E");
-        randomizedQueue.enqueue("F");
-        randomizedQueue.enqueue("G");
-        randomizedQueue.enqueue("H");
-        randomizedQueue.enqueue("I");
+        String string = "ABCDEFGHI";
+        for (char ch : string.toCharArray()) {
+            randomizedQueue.enqueue(ch + "");
+        }
 
         for (int i = 0; i < 3; i++) System.out.println(randomizedQueue.dequeue());
 
