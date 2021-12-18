e710cfe1468e23157efa7203fec8f6a3d6d85ff4
Ali Asadi <aliesaassadi@gmail.com>
2021-12-18 19:29:13 +0200

    update

--- src/algorithms/sort/questions/KthLargestElementInArray.java
+++ src/algorithms/sort/questions/KthLargestElementInArray.java
@@ -44,6 +44,6 @@ public class KthLargestElementInArray {
 
     public static void main(String[] args) {
         int[] arr = {3, 2, 1, 5, 6, 4};
-        System.out.println(findKthLargestPriorityQueue(arr,2));
+        System.out.println(findKthLargest(arr,2));
     }
 }
