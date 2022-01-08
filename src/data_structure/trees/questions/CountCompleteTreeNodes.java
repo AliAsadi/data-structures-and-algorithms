ac43012f81d74fb86044367379d19398c6eaca02
Ali Asadi <aliesaassadi@gmail.com>
2022-01-08 22:45:13 +0200

    update

---
+++ src/data_structure/trees/questions/CountCompleteTreeNodes.java
@@ -0,0 +1,28 @@
+package data_structure.trees.questions;
+
+import data_structure.trees.utils.TreeNode;
+
+/**
+ * Given the root of a complete binary tree, return the number of the nodes in the tree.
+ *
+ * According to Wikipedia, every level, except possibly the last, is completely filled
+ * in a complete binary tree, and all nodes in the last level are as far left as possible.
+ * It can have between 1 and 2h nodes inclusive at the last level h.
+ *
+ * Design an algorithm that runs in less than O(n) time complexity.
+ *
+ * Input: root = [1,2,3,4,5,6]
+ * Output: 6
+ *
+ * https://leetcode.com/problems/count-complete-tree-nodes/
+ */
+public class CountCompleteTreeNodes {
+
+    public static int countNodes(TreeNode root) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
