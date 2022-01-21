68c7a4a80c327f7150726425ed629b7b8defaf3c
Ali Asadi <aliesaassadi@gmail.com>
2022-01-21 23:58:13 +0200

    update

--- src/data_structure/arrays/questions/matrices/NumberOfIslands.java
+++ src/data_structure/arrays/questions/matrices/NumberOfIslands.java
@@ -1,7 +1,7 @@
 package data_structure.arrays.questions.matrices;
 
 /**
- * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water),
+ * Given an m x n 2D binary grid which represents a map of '1's (land) and '0's (water),
  * return the number of islands.
  *
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
