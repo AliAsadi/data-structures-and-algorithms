4b268c85f867e67aaab049e2590c36461859a759
Ali Asadi <aliesaassadi@gmail.com>
2022-01-21 23:57:56 +0200

    update

---
+++ src/data_structure/arrays/questions/matrices/NumberOfIslands.java
@@ -0,0 +1,30 @@
+package data_structure.arrays.questions.matrices;
+
+/**
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water),
+ * return the number of islands.
+ *
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Input: grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ *
+ * https://leetcode.com/problems/number-of-islands/
+ */
+public class NumberOfIslands {
+
+    public static int numIslands(char[][] grid) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
