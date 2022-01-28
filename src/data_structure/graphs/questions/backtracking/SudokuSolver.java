d743e2550b42f40a58b119f51969d54a3c02ad42
Ali Asadi <aliesaassadi@gmail.com>
2022-01-28 22:24:54 +0200

    update

---
+++ src/data_structure/graphs/questions/backtracking/SudokuSolver.java
@@ -0,0 +1,36 @@
+package data_structure.graphs.questions.backtracking;
+
+/**
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * <p>
+ * A sudoku solution must satisfy all of the following rules:
+ * <p>
+ * Each of the digits 1-9 must occur exactly once in each row.
+ * Each of the digits 1-9 must occur exactly once in each column.
+ * Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+ * The '.' character indicates empty cells.
+ * <p>
+ * https://leetcode.com/problems/sudoku-solver/
+ */
+public class SudokuSolver {
+
+    public static void solveSudoku(char[][] board) {
+
+    }
+
+    public static void main(String[] args) {
+
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+    }
+}
