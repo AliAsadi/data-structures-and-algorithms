2c4e86aa33bab8820fcae01971c0b4013ff34bc5
Ali Asadi <aliesaassadi@gmail.com>
2022-10-16 17:30:12 +0300

    update

--- src/algorithms/dynamic_programming/questions/MinCostClimbingStairs.java
+++ src/algorithms/dynamic_programming/questions/MinCostClimbingStairs.java
@@ -28,15 +28,28 @@ package algorithms.dynamic_programming.questions;
  * - Pay 1 and climb one step to reach the top.
  * The total cost is 6.
  * <p>
+ * Constraints:
+ * 2 <= cost.length <= 1000
+ * 0 <= cost[i] <= 999
+ * <p>
  * https://leetcode.com/problems/min-cost-climbing-stairs/
  */
 public class MinCostClimbingStairs {
 
-    public int minCostClimbingStairs(int[] cost) {
-        return 0;
+    /**
+     * Time: O(n)
+     * Space: O(1)
+     **/
+    public static int minCostClimbingStairs(int[] cost) {
+        for (int i = 2; i < cost.length; i++) {
+            cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+        }
+        return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
     }
 
     public static void main(String[] args) {
-
+//        int min = minCostClimbingStairs(new int[]{10, 15, 20});
+        int min = minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1});
+        System.out.println(min);
     }
 }
