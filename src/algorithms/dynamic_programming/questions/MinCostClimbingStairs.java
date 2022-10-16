29168a65facac031759869138502b93ba19341b8
Ali Asadi <aliesaassadi@gmail.com>
2022-10-16 14:13:48 +0300

    update

---
+++ src/algorithms/dynamic_programming/questions/MinCostClimbingStairs.java
@@ -0,0 +1,42 @@
+package algorithms.dynamic_programming.questions;
+
+/**
+ * You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+ * <p>
+ * You can either start from the step with index 0, or the step with index 1.
+ * <p>
+ * Return the minimum cost to reach the top of the floor.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: cost = [10,15,20]
+ * Output: 15
+ * Explanation: You will start at index 1.
+ * - Pay 15 and climb two steps to reach the top.
+ * The total cost is 15.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: cost = [1,100,1,1,1,100,1,1,100,1]
+ * Output: 6
+ * Explanation: You will start at index 0.
+ * - Pay 1 and climb two steps to reach index 2.
+ * - Pay 1 and climb two steps to reach index 4.
+ * - Pay 1 and climb two steps to reach index 6.
+ * - Pay 1 and climb one step to reach index 7.
+ * - Pay 1 and climb two steps to reach index 9.
+ * - Pay 1 and climb one step to reach the top.
+ * The total cost is 6.
+ * <p>
+ * https://leetcode.com/problems/min-cost-climbing-stairs/
+ */
+public class MinCostClimbingStairs {
+
+    public int minCostClimbingStairs(int[] cost) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
